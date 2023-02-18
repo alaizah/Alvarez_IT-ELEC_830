7ada68dd95b1543f73d81c458506a1e903c0a6a6
Admin <alvarezalaizah@gmail.com>
2023-02-18 13:06:47 +0800

    update

--- app/src/main/java/com/example/activityalvarez/MainActivity.kt
+++ app/src/main/java/com/example/activityalvarez/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.activityalvarez
 
+import android.content.ContentValues.TAG
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.v(TAG, "This is a verbose log.")
+        Log.d(TAG, "This is a debug log.")
+        Log.i(TAG, "This is an info log.")
+        Log.w(TAG, "This is a warn log.")
+        Log.e(TAG, "This is an error log.")
+
+        Button button = (Button) findViewbyId(R.id.button2)
+        button.setOnClickListener(new View.OnClickListener())
     }
 }
